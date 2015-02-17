9c0f04d7c7d80da68c681dfec2c0f7ad4f4d605a
Gabor Szabo <gabor@szabgab.com>
2015-02-17 17:36:40 +0200

    show the results

--- poll.py
+++ poll.py
@@ -22,6 +22,20 @@ def poll():
 
     return render_template('thankyou.html', data=poll_data)
 
+@app.route('/results')
+def show_results():
+    votes = {}
+    for f in poll_data['fields']:
+        votes[f] = 0
+
+    f  = open(filename, 'r')
+    for line in f:
+        vote = line.rstrip("\n")
+        votes[vote] += 1
+
+    return render_template('results.html', data=poll_data, votes=votes)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
